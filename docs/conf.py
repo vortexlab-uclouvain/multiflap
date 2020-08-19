06093c6967a121743f2f2f15fec4342c4ffd4bf5
Gianmarco Ducci <gianmarco.ducci@uclouvain.be>
2020-08-19 17:04:08 +0200

    fd

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 
 #
-html_theme = 'nature'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
