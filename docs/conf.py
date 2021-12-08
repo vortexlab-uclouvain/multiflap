b3cce1d0c366576e8f02e840ddaa3c60667d7d6f
Gianmarco Ducci <gianmarco.ducci@uclouvain.be>
2021-12-08 16:16:58 +0100

    corrected reqs

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ release = '1.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 #extensions = [ 'sphinx.ext.napoleon','sphinx.ext.coverage', 'sphinxcontrib.bibtex', 'sphinxcontrib.programoutput', 'sphinx.ext.todo', 'sphinx.ext.viewcode','sphinx.ext.autodoc']
-extensions = ["sphinx.ext.napoleon", "sphinx.ext.viewcode", 'sphinx.ext.autodoc', 'sphinxcontrib.contentui']
+extensions = ["sphinx.ext.napoleon", "sphinx.ext.viewcode", 'sphinx.ext.autodoc']
 
 autoapi_dirs = ["../multiflap"]
 autoapi_file_patterns = ["*.py"]
